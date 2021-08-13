bc11cb9cf27cb50db23c01246acddb85e1eaf51a
manikandarajar.20mca@kongu.edu <manikandanraja1204@gmail.com>
2021-08-13 14:10:05 +0530

    updated

--- src/components/PropsAndStates.js
+++ src/components/PropsAndStates.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const PropsAndStates = (props) => {
-     const {name, heroName} = props;
-     return (
-          <div>
-               <h1>Hello {name} works as {heroName}</h1>
-          </div>
-     )
+export class PropsAndStates extends Component {
+
+
+     render() {
+          return (
+               <div>
+                    <h1>Maniraj</h1>
+               </div>
+          )
+     }
 }
 
 export default PropsAndStates
